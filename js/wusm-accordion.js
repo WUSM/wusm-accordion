be6b4ba99ba782d2ecdb417401c90386cbfe2ee3
Aaron Graham <aegraham@gmail.com>
2014-01-29 15:31:27 -0600

    fixed it so only one is open at a time

--- js/wusm-accordion.js
+++ js/wusm-accordion.js
@@ -5,31 +5,30 @@ jQuery(document).ready(function($) {
 		});
 
 		$('.accordion-header').click(function() {
-            var $this = $(this);
-            if ($this.next('.accordion-body-text').is(':hidden')) {
-                $this.find('.d1').animate({ top: 40 });
-                $this.next('.accordion-body-text').slideDown('fast');
-                $this.addClass('open');
-            } else {
-                $('.d1').animate({ top: 0 });
-                $('.accordion-header').removeClass('open');
-                $('.accordion-body-text').slideUp('fast');
-            }
+			$('.open').find('.d1').animate({ top: 0 });
+			$('.open').next('.accordion-body-text').slideUp('fast');
+			$('.open').removeClass('open');
+			var $this = $(this);
+			if ($this.next('.accordion-body-text').is(':hidden')) {
+				$this.find('.d1').animate({ top: 50 });
+				$this.next('.accordion-body-text').slideDown('fast');
+				$this.addClass('open');
+			}
 		});
 
 		$('.expand-all').click(function() {
-            if ( $(this).html() === 'Expand all' ) {
-                $(this).html('Collapse all');
-                $('.accordion-body-text').slideDown('fast');
-                $('.accordion-body-text').addClass('open');
-                $('.d1').animate({ top: 40 });
-            } else {
-                $(this).html('Expand all');
-                $('.accordion-body-text').slideUp('fast');
-                $('.accordion-body-text').removeClass('open');
-                $('.d1').animate({ top: 0 });
+			if ( $(this).html() === 'Expand all' ) {
+				$(this).html('Collapse all');
+				$('.accordion-body-text').slideDown('fast');
+				$('.accordion-body-text').addClass('open');
+				$('.d1').animate({ top: 50 });
+			} else {
+				$(this).html('Expand all');
+				$('.accordion-body-text').slideUp('fast');
+				$('.accordion-body-text').removeClass('open');
+				$('.d1').animate({ top: 0 });
 
-            }
+			}
 		});
 	}
 });
